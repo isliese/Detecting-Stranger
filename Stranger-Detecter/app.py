f63bfff0369aa5adab1a087f782e56b1da8885d0
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-07-15 02:17:09 +0900

    the first commit

--- Stranger-Detecter/app.py
+++ Stranger-Detecter/app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('Home1.html')
 
-@app.route('/')
-def index():
+@app.route('/Home2')
+def home2():
     return render_template('Home2.html')
+
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
+
+@app.route('/SignUp')
+def signup():
+    return render_template('SignUp.html')
+
+@app.route('/EmptyCamSelect')
+def emptycamselect():
+    return render_template('EmptyCamSelect.html')
+
+@app.route('/CamSelect')
+def camselect():
+    return render_template('CamSelect.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
