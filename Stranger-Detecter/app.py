a19bec6dda9678d2cf663e0a4c6f56c31db988c4
Yeonjin0121 <165582326+Yeonjin0121@users.noreply.github.com>
2024-07-24 13:11:17 +0900

    Add files via upload

---
+++ Stranger-Detecter/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('Home1.html')
+
+@app.route('/Home2')
+def home2():
+    return render_template('Home2.html')
+
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
+
+@app.route('/SignUp')
+def signup():
+    return render_template('SignUp.html')
+
+@app.route('/EmptyCamSelect')
+def emptycamselect():
+    return render_template('EmptyCamSelect.html')
+
+@app.route('/CamSelect')
+def camselect():
+    return render_template('CamSelect.html')
+
+@app.route('/EmptyCam1Select')
+def emptycam1select():
+    return render_template('EmptyCam1Select.html')
+
+@app.route('/EmptyCam2Select')
+def emptycam2select():
+    return render_template('EmptyCam2Select.html')
+
+@app.route('/Cam2')
+def cam2():
+    return render_template('Cam2.html')
+
+@app.route('/RegisteredCard')
+def registeredcard():
+    return render_template('RegisteredCard.html')
+
+@app.route('/EditCard')
+def editcard():
+    return render_template('EditCard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
