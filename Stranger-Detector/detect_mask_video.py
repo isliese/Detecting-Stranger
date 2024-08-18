7281328bd9a40ec4f5fd0408c07c0a6f06bc2956
hyunchin2 <hyun0325@sookmyung.ac.kr>
2024-08-18 12:27:50 +0900

    미등록자 한글로 띄우기

--- Stranger-Detector/detect_mask_video.py
+++ Stranger-Detector/detect_mask_video.py
@@ -7,8 +7,10 @@ from tensorflow.keras.models import load_model
 from imutils.video import VideoStream
 import imutils
 import time
+from PIL import ImageFont, ImageDraw, Image
 import custom_layers
 
+system_font_path = "/Users/leehyunchin/Downloads/Nanum_Gothic/NanumGothic-Regular.ttf"
 
 # LabelEncoder 객체 로드
 with open('label_encoder.pkl', 'rb') as f:
@@ -50,6 +52,19 @@ def detect_and_predict_celebrity(frame, faceNet, celebrityNet, lb):
 
     return (locs, preds)
 
+def put_korean_text(image, text, position, font_path=system_font_path, font_size=20, color=(255, 255, 255)):
+    try:
+        font = ImageFont.truetype(font_path, font_size)
+    except OSError:
+        print(f"Warning: '{font_path}' 폰트를 찾을 수 없습니다. 기본 폰트를 사용합니다.")
+        font = ImageFont.load_default()  # 기본 폰트 사용
+
+    image_pil = Image.fromarray(image)
+    draw = ImageDraw.Draw(image_pil)
+    draw.text(position, text, font=font, fill=color)
+    image = np.array(image_pil)
+    return image
+
 def generate_frames():
     # 얼굴 탐지 모델 로드
     prototxtPath = "face_detector/deploy.prototxt"
@@ -72,15 +87,21 @@ def generate_frames():
             (startX, startY, endX, endY) = box
             cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)  # 검출된 얼굴 영역 표시
             max_index = np.argmax(pred)
-            label = lb.classes_[max_index]
             confidence = pred[max_index]
 
-            # 예측 확률이 0.4 이상일 경우에만 레이블 표시
-            if confidence > 0.4:
+            # 예측 확률이 0.5 이상일 경우에만 레이블 표시
+            if confidence > 0.5:
+                label = lb.classes_[max_index]
                 color = (0, 255, 0)
-                label = "{}: {:.2f}%".format(label, confidence * 100)
-                cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                label_text = "{}: {:.2f}%".format(label, confidence * 100)
+                
+            else:
+                label_text = "미등록자"
+                color = (0, 0, 255)
+                
+            # 한국어 텍스트를 이미지에 추가
+            frame = put_korean_text(frame, label_text, (startX, startY - 30), font_size=20, color=color)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
         # 프레임을 JPEG로 인코딩
         ret, buffer = cv2.imencode('.jpg', frame)
